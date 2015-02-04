f49513bee97fbf7ed2c414d36ba16b2d681276af
Ciaran OConnor <ciaranoconnor23@gmail.com>
2015-02-04 23:04:54 +0000

    initial commit. Start of login

---
+++ app/controllers/Application.java
@@ -0,0 +1,63 @@
+package controllers;
+
+import models.User;
+import play.*;
+import play.api.libs.ws.ssl.SystemConfiguration;
+import play.data.Form;
+import play.mvc.*;
+import play.Logger;
+
+import views.html.*;
+
+import java.util.List;
+
+import static play.data.Form.form;
+
+public class Application extends Controller {
+
+    public static class Login {
+
+        public String email;
+        public String password;
+
+        public String validate() {
+            if (User.authenticate(email, password) == null) {
+                return "Invalid user or password";
+            }
+            return null;
+        }
+
+    }
+
+    public static Result index() {
+        List<User> users = User.find.all();
+        Logger.info(""+users.get(0).email);
+        return ok(index.render("Your new application is ready."));
+    }
+
+    public static Result login(){
+        return ok(login.render(form(Login.class)));
+    }
+
+    public static Result authenticate() {
+        Form<Login> loginForm = form(Login.class).bindFromRequest();
+        if (loginForm.hasErrors()) {
+            return badRequest(login.render(loginForm));
+        } else {
+            session().clear();
+            session("email", loginForm.get().email);
+            return redirect(
+                    routes.Application.index()
+            );
+        }
+    }
+
+    public static Result logout() {
+        session().clear();
+        flash("success", "You've been logged out");
+        return redirect(
+                routes.Application.login()
+        );
+    }
+
+}
---
+++ app/models/User.java
@@ -0,0 +1,36 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+
+import play.db.ebean.*;
+import play.data.validation.*;
+
+@Entity
+public class User extends Model {
+
+    User(String email, String name, String password){
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+    //play framework changes these to private and adds getters + setters on run
+    @Id
+    public Long id;
+
+    @Constraints.Email
+    public String email;
+
+    @Constraints.Required
+    public String name;
+
+    @Constraints.Required
+    public String password;
+
+    public static Finder<Long, User> find = new Finder<Long,User>(Long.class, User.class);
+
+    public static User authenticate(String email, String password) {
+        return find.where().eq("email", email)
+                .eq("password", password).findUnique();
+    }
+}
