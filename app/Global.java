0b703b743457b065b5f9b2b3abbf4ac7a71f8bd4
Ciaran O'Connor <ciaranoconnor23@gmail.com>
2015-02-17 22:45:25 +0000

    404 closes #22

--- app/Global.java
+++ app/Global.java
@@ -1,10 +1,22 @@
 import play.GlobalSettings;
 import play.api.mvc.EssentialFilter;
 import play.filters.csrf.CSRFFilter;
+import play.libs.F;
+import play.mvc.Http;
+import play.mvc.Result;
+
+import static play.mvc.Results.notFound;
 
 public class Global extends GlobalSettings {
     @Override
     public <T extends EssentialFilter> Class<T>[] filters() {
         return new Class[]{CSRFFilter.class};
     }
+
+    public F.Promise<Result> onHandlerNotFound(Http.RequestHeader request){
+        return F.Promise.<Result>pure(notFound(
+                views.html.notFoundPage.render()
+        ));
+    }
+
 }
